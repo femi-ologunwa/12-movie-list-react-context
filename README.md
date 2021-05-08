48ed2396ff2e20b12b2b9ee090cc4a85fb1f6763
Femi <ologunwafemi@gmail.com>
2021-05-09 00:44:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### React Context For State Management
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+   return (
+      <div className='App'>
+         <h3>movies</h3>
+      </div>
+   );
+}
+
+export default App;
