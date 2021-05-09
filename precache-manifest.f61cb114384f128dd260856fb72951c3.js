49cf21bfe0a094dda475c153a556e5e1e3119345
Femi <ologunwafemi@gmail.com>
2021-05-09 08:11:25 +0100

    Updates

---
+++ precache-manifest.f61cb114384f128dd260856fb72951c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78878672b0508b62969247c58dc533ed",
+    "url": "/12-movie-list-react-context/index.html"
+  },
+  {
+    "revision": "13810d3650c70057de96",
+    "url": "/12-movie-list-react-context/static/css/main.526093bf.chunk.css"
+  },
+  {
+    "revision": "3ec3d1b5a9ffbb40ffa8",
+    "url": "/12-movie-list-react-context/static/js/2.676e622f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/12-movie-list-react-context/static/js/2.676e622f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13810d3650c70057de96",
+    "url": "/12-movie-list-react-context/static/js/main.5a98c1e8.chunk.js"
+  },
+  {
+    "revision": "d5367220bc153d3dff95",
+    "url": "/12-movie-list-react-context/static/js/runtime-main.f15e875c.js"
+  }
+]);
